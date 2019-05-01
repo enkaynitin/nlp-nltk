eb4bedd39e1d5945b0e33b148fd07e31f23e2e70
Nitin Kumar <nitinkumarbnk@gmail.com>
2019-05-02 00:05:51 +0530

    Adding tokenize and stop words

---
+++ nltktokenize.py
@@ -0,0 +1,16 @@
+from nltk.tokenize import sent_tokenize,  word_tokenize
+
+
+# tokenizing
+	#word
+	#sentence
+
+# corpora : body of text. ex: journal, speech, English language
+
+# Lexicon - words and their meaning
+
+example_text = "Hello Mr. Smith, how are you doing today? The weather is great and python is awesome. The sky is pinkish-blue. You should not eat cardboard."
+
+print(sent_tokenize(example_text))
+
+
